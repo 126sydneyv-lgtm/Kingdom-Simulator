43529e729957d545942d65ad9719ebcbd0fc3631
Sydney Valliere <126.sydney.v@gmail.com>
2026-01-09 11:28:37 -0500

    Initial project setup and variables

--- Kingdom Simulator/Form1.Designer.cs
+++ Kingdom Simulator/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(500, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(435, 281);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Kingdom Simulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Kingdom Simulator/Form1.cs
+++ Kingdom Simulator/Form1.cs
@@ -12,9 +12,55 @@ namespace Kingdom_Simulator
 {
     public partial class Form1 : Form
     {
+        //kingdom stats
+        int resources = 100;
+        int riches = 100;
+        int population = 100;
+        int happiness = 100;
+
+        //game control
+        int currentYear = 1;
+        int maxYears = 10;
+        bool gameOver = false;
+
+        //lists
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+       void Main(string[] args)
+        {
+           
+
+            DisplayTitleScreen();
+            InitializeKingdom();
+
+            //main game loop
+            while (!gameOver && currentYear <= maxYears)
+            {
+
+            }
+        }
+        //methods
+
+        //displays "title screen" of the program.
+        void DisplayTitleScreen()
+        {
+            label1.Text = "KINGDOM SIMULATOR";
+            
+        }
+
+        //sets up kingdom at the start of the game
+        void InitializeKingdom()
+        {
+           
+            label1.Text = "Enter the name of your kingdom: ";
+            string kingdomName = Convert.ToString(textBox1.Text);
+
+            label1.Text = "\n" + "Welcome, ruler of " + kingdomName + "!";
+            label1.Text = "Your reign begins now...";
         }
     }
 }
